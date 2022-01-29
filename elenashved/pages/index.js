fa8056f11a7a2b535f6bced25f822ec1286a2b38
Luka Ponomarenko <luka.ponomarenko@gmail.com>
2022-01-29 10:48:34 +0200

    upload images

--- elenashved/pages/index.js
+++ elenashved/pages/index.js
@@ -1,7 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <div className={styles.app}></div>;
 }
---
+++ elenashved/pages/publications/index.js
@@ -0,0 +1,3 @@
+export default function Publications() {
+  return <h1>Publications</h1>;
+}
