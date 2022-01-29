e4635db58cf215e668edba2fb423edd9a3ee8592
Luka Ponomarenko <luka.ponomarenko@gmail.com>
2022-01-29 18:07:00 +0200

    microfix

--- elenashved/pages/_app.js
+++ elenashved/pages/_app.js
@@ -1,18 +1,9 @@
 import Header from "../components/Header/Header";
-import Head from "next/head";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Header />
       <Component {...pageProps} />
     </>
