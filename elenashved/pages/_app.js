78f09c17663450e06e0d3cb10c9a3c6187cdf0e5
Luka Ponomarenko <luka.ponomarenko@gmail.com>
2022-02-01 16:04:21 +0200

    fix

--- elenashved/pages/_app.js
+++ elenashved/pages/_app.js
@@ -7,7 +7,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600;700&display=swap"
           rel="stylesheet"
