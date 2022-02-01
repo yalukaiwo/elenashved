17fde5690bddfe44b1e4ba004aeb9fc384c616de
Luka Ponomarenko <luka.ponomarenko@gmail.com>
2022-02-01 15:54:32 +0200

    menu fix

--- elenashved/components/Header/Header.js
+++ elenashved/components/Header/Header.js
@@ -101,134 +101,124 @@ export default function Header() {
               burger ? styles.burger__content_holder_active : ""
             }`}
             onClick={backgroundClickHandler}
+          ></section>
+          <div
+            className={`${styles.burger__content} ${
+              burger ? styles.burger__content_active : ""
+            }`}
           >
-            <div
-              className={`${styles.burger__content} ${
-                burger ? styles.burger__content_active : ""
-              }`}
-            >
-              <div className={styles.burger__header}>
-                <div className={styles.burger} onClick={burgerClickHandler}>
-                  <Image src="/socials/close.png" width={20} height={20} />
-                </div>
-              </div>
-              <ul className={styles.burger__nav}>
-                <li className={styles.nav__link}>
-                  <Link href="/about">
-                    <a
-                      className={`${styles.nav__link_a} ${
-                        styles.burger__link
-                      } ${
-                        router.pathname === "/about"
-                          ? styles.nav__link_a__active
-                          : ""
-                      }`}
-                    >
-                      About
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.nav__link}>
-                  <Link href="/gallery">
-                    <a
-                      className={`${styles.nav__link_a} ${
-                        styles.burger__link
-                      } ${
-                        router.pathname === "/gallery"
-                          ? styles.nav__link_a__active
-                          : ""
-                      }`}
-                    >
-                      Gallery
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.nav__link}>
-                  <Link href="/books">
-                    <a
-                      className={`${styles.nav__link_a} ${
-                        styles.burger__link
-                      } ${
-                        router.pathname === "/books"
-                          ? styles.nav__link_a__active
-                          : ""
-                      }`}
-                    >
-                      Books
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.nav__link}>
-                  <Link href="/contact">
-                    <a
-                      className={`${styles.nav__link_a} ${
-                        styles.burger__link
-                      } ${
-                        router.pathname === "/contact"
-                          ? styles.nav__link_a__active
-                          : ""
-                      }`}
-                    >
-                      Contact
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-              <ul className={styles.burger__gallery_nav}>
-                <li className={styles.burger__gallery_link}>
-                  <Link href="/gallery/behind-the-red-line">
-                    <a
-                      className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
-                    >
-                      Behind The Red Line
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.burger__gallery_link}>
-                  <Link href="/gallery/the-eidetics-of-burnout-syndrome">
-                    <a
-                      className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
-                    >
-                      The Eidetics Of Burnout Syndrome
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.burger__gallery_link}>
-                  <Link href="/gallery/city-patchwork">
-                    <a
-                      className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
-                    >
-                      City Patchwork
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.burger__gallery_link}>
-                  <Link href="/gallery/the-habitat">
-                    <a
-                      className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
-                    >
-                      The Habitat
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-              <div className={`${styles.socials} ${styles.burger__contacts}`}>
-                <a href="https://www.instagram.com/shved_artphoto/">
-                  <Image
-                    src={"/socials/instagram.png"}
-                    width={25}
-                    height={25}
-                  />
-                </a>
-                <a href="https://www.instagram.com/shved_artphoto/">
-                  <Image src={"/socials/facebook.png"} width={25} height={25} />
-                </a>
-                <a href="mailto:Lenashved@ukr.net">
-                  <Image src={"/socials/email.png"} width={25} height={25} />
-                </a>
+            <div className={styles.burger__header}>
+              <div
+                className={`${styles.burger} ${styles.burger__open}`}
+                onClick={burgerClickHandler}
+              >
+                <Image src="/socials/close.png" width={20} height={20} />
               </div>
             </div>
-          </section>
+            <ul className={styles.burger__nav}>
+              <li className={styles.nav__link}>
+                <Link href="/about">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__link} ${
+                      router.pathname === "/about"
+                        ? styles.nav__link_a__active
+                        : ""
+                    }`}
+                  >
+                    About
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.nav__link}>
+                <Link href="/gallery">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__link} ${
+                      router.pathname === "/gallery"
+                        ? styles.nav__link_a__active
+                        : ""
+                    }`}
+                  >
+                    Gallery
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.nav__link}>
+                <Link href="/books">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__link} ${
+                      router.pathname === "/books"
+                        ? styles.nav__link_a__active
+                        : ""
+                    }`}
+                  >
+                    Books
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.nav__link}>
+                <Link href="/contact">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__link} ${
+                      router.pathname === "/contact"
+                        ? styles.nav__link_a__active
+                        : ""
+                    }`}
+                  >
+                    Contact
+                  </a>
+                </Link>
+              </li>
+            </ul>
+            <ul className={styles.burger__gallery_nav}>
+              <li className={styles.burger__gallery_link}>
+                <Link href="/gallery/behind-the-red-line">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
+                  >
+                    Behind The Red Line
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.burger__gallery_link}>
+                <Link href="/gallery/the-eidetics-of-burnout-syndrome">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
+                  >
+                    The Eidetics Of Burnout Syndrome
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.burger__gallery_link}>
+                <Link href="/gallery/city-patchwork">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
+                  >
+                    City Patchwork
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.burger__gallery_link}>
+                <Link href="/gallery/the-habitat">
+                  <a
+                    className={`${styles.nav__link_a} ${styles.burger__gallery_link_a}`}
+                  >
+                    The Habitat
+                  </a>
+                </Link>
+              </li>
+            </ul>
+            <div className={`${styles.socials} ${styles.burger__contacts}`}>
+              <a href="https://www.instagram.com/shved_artphoto/">
+                <Image src={"/socials/instagram.png"} width={25} height={25} />
+              </a>
+              <a href="https://www.instagram.com/shved_artphoto/">
+                <Image src={"/socials/facebook.png"} width={25} height={25} />
+              </a>
+              <a href="mailto:Lenashved@ukr.net">
+                <Image src={"/socials/email.png"} width={25} height={25} />
+              </a>
+            </div>
+          </div>
         </section>
       </div>
     </>
